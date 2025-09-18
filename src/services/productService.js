0eb501a5068b5d717d64ee2469b826c0c0f1e5c6
Dunaka Chetan <chetan521230@gmail.com>
2025-09-18 11:45:08 +0530

    Update productService.js

--- src/services/productService.js
+++ src/services/productService.js
@@ -15,5 +15,5 @@ export const getProducts = async (category = "") => {
 
 // Get product image URL
 export const getProductImageUrl = (imagePath) => {
-  return `http://localhost:8080/api/products/images/${imagePath}`;
+  return `http://ec2-3-95-9-156.compute-1.amazonaws.com:8080/api/products/images/${imagePath}`;
 };
